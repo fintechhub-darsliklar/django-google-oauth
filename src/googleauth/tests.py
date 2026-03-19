852c7a7307c493430e6256cac642591a007ecf62
ziyodullo <ziyodullodev@gmail.com>
2026-03-19 13:36:16 +0500

    add: test case and remove print()

--- src/googleauth/tests.py
+++ src/googleauth/tests.py
@@ -3,27 +3,19 @@ from django.contrib.auth.models import User
 
 # Create your tests here.
 
+
 class LoginTest(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(
-            username="testuser",
-            password="12345"
-        )
+        self.user = User.objects.create_user(username="testuser", password="12345")
 
     def test_login(self):
-        logged_in = self.client.login(
-            username="testuser",
-            password="12345"
-        )
+        logged_in = self.client.login(username="testuser", password="12345")
         self.assertTrue(logged_in)
 
 
 class DashboardTest(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(
-            username="testuser",
-            password="12345"
-        )
+        self.user = User.objects.create_user(username="testuser", password="12345")
 
     def test_dashboard_page(self):
         response = self.client.get("/")
@@ -33,4 +25,3 @@ class DashboardTest(TestCase):
         self.client.login(username="testuser", password="12345")
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-
